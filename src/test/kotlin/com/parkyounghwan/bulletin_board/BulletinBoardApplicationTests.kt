e55fc04463c4edde783c410ac4a38889b2e9dfb7
f-lab-pig <pig@f-lab.kr>
2025-03-13 16:50:02 +0900

    project setting

---
+++ src/test/kotlin/com/parkyounghwan/bulletin_board/BulletinBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.parkyounghwan.bulletin_board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BulletinBoardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
